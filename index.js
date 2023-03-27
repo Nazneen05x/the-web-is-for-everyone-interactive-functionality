ebbf23e39c7407c4395b974b7f673ef1ef095d06
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-03-27 14:03:53 +0200

    post bijna i think

--- index.js
+++ index.js
@@ -18,6 +18,45 @@ server.use(express.urlencoded({ extended: true }))
 
 //hier komen de routes
 
+server.get('/', (request, response) => {
+  const baseUrl = "https://api.vinimini.fdnd.nl/api/v1/"
+  const pepijnId = "notities?id=clemozv3c3eod0bunahh71sx7"
+  const url = `${baseUrl}${pepijnId}`
+
+  fetchJson(url).then((data) => {
+    response.render('index', data)
+  })
+})
+
+// Dit is de data uit whois api // Met deze stuk code willen we de data roepen uit de whois api?
+server.post('/', function (req, res, next) {
+  const baseurl = "https://api.vinimini.fdnd.nl/api/v1/"
+  const url = `${baseurl}`
+  req.body.afgerond = false
+  req.body.persoonId = 'clemozv3c3eod0bunahh71sx7'
+  req.body.datum = req.body.datum + ':00Z';
+  req.body.herinnering = [req.body.herinnering + ':00Z']
+  console.log(req.body)
+
+
+  postJson(url + '/notities', req.body).then((data) => {
+    console.log(JSON.stringify(data))
+    let newNotitie = { ... req.body }
+
+    if (data.success) {
+      res.redirect('/new') 
+      // TODO: squad meegeven, message meegeven
+      // TODO: Toast meegeven aan de homepagina
+    } else {
+      const errormessage = `${data.message}: Mogelijk komt dit door de slug die al bestaat.`
+      const newdata = { error: errormessage, values: newData }
+      
+      res.render('index', newdata)
+    }
+  })
+})
+
+
 
 
 
@@ -25,9 +64,9 @@ server.use(express.urlencoded({ extended: true }))
 
 
 // Stel het poortnummer in en start express
-app.set('port', process.env.PORT || 8000)
-app.listen(app.get('port'), function () {
-  console.log(`Application started on http://localhost:${app.get('port')}`)
+server.set('port', process.env.PORT || 8000)
+server.listen(server.get('port'), function () {
+  console.log(`Application started on http://localhost:${server.get('port')}`)
 })
 
 
