4e96e18f45f4031ea8ef7892999d1be4fe5d7861
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2023-03-31 00:22:43 +0200

    new new

---
+++ public/script.js
@@ -0,0 +1,17 @@
+const iconcross = document.querySelector(".iconcross")
+const form = document.querySelector("form")
+
+iconcross.addEventListener("click", popup)
+
+
+function popup(){
+    form.classList.add("form-visibility")
+    }
+
+    const kruis =document.querySelector(".kruis")
+
+    kruis.addEventListener("click",shoutdown)
+    
+    function shoutdown(){
+    form.classList.remove("form-visibility")
+    }
